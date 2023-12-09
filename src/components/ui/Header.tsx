cb1501aa9cdbf9fcadef7f923b6a957733a17821
Rogerio <rogeriopiatek@gmail.com>
2023-12-08 21:35:23 -0300

    chore: ensure prisma has the needed Post model

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,21 +3,37 @@
 import { ReactNode } from "react";
 import { Separator } from "./separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import { Button } from "./button";
 
 const Header = () => {
-  const session = useSession();
-  console.log(session);
+  const { status, data } = useSession();
+
+  const handleSignIn = () => {
+    signIn();
+  };
+
+  console.log(status);
   return (
     <div>
       <div className="text-xl leading-heading font-semibold w-full bg-tuna-950 h-14 shadow-sm  p-2 flex justify-between items-center">
         <div>Posts</div>
         <div>
-          {" "}
-          <Avatar>
-            <AvatarImage src="https://github.com/rogeriopiatek.png" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+          {status === "authenticated" ? (
+            <Avatar>
+              <AvatarImage
+                src={
+                  data.user?.image ?? "../../../../public/default_avatar.jpg"
+                }
+                alt="User image"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          ) : (
+            <Button className="bg-tuna-50 text-tuna-950" onClick={handleSignIn}>
+              Sign in
+            </Button>
+          )}
         </div>
       </div>
       <Separator className="bg-tuna-800" />
@@ -25,6 +41,7 @@ const Header = () => {
   );
 };
 
-//TODO: Add clicking Posts makes page refresh, and authentication with Next Auth
+//TODO: Add clicking Posts makes page refresh
+//TODO: Add sign-out option via side menu or modal
 
 export default Header;

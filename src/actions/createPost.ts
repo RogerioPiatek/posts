a0844770e2ed29a3ece5404b4e65fc9b11138a83
Rogerio <rogeriopiatek@gmail.com>
2023-12-12 00:03:41 -0300

    chore: ensure name is properly stored in the DB

--- src/actions/createPost.ts
+++ src/actions/createPost.ts
@@ -5,11 +5,17 @@ import prisma from "@/lib/prisma";
 interface createPostProps {
   email: string;
   postContent: string;
+  name: string;
 }
 
-export const createPost = async ({ postContent, email }: createPostProps) => {
+export const createPost = async ({
+  postContent,
+  email,
+  name,
+}: createPostProps) => {
   await prisma.post.create({
     data: {
+      name: name,
       authorId: email,
       content: postContent,
     },

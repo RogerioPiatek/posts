72087862521cb4581aa9fd7555b063065f3a1b49
Rogerio <rogeriopiatek@gmail.com>
2023-12-14 21:30:48 -0300

    feat: ensure user can logout

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -3,8 +3,9 @@
 import { ReactNode } from "react";
 import { Separator } from "./separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "./button";
+import { LogOutIcon } from "lucide-react";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -13,22 +14,29 @@ const Header = () => {
     signIn();
   };
 
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <div>
       <div className="text-xl leading-heading font-semibold w-full bg-tuna-950 shadow-sm  p-2 flex justify-between items-center">
         <div>Posts</div>
         <div>
           {status === "authenticated" ? (
-            <Avatar>
-              <AvatarImage
-                src={
-                  data.user?.image ?? "../../../../public/default_avatar.jpg"
-                }
-                alt="User image"
-                className="h-10 w-10"
-              />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
+            <div className="flex flex-row-reverse items-center gap-3">
+              <Avatar>
+                <AvatarImage
+                  src={
+                    data.user?.image ?? "../../../../public/default_avatar.jpg"
+                  }
+                  alt="User image"
+                  className="h-10 w-10"
+                />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+              <LogOutIcon onClick={handleLogout} className="text-tuna-600" />
+            </div>
           ) : (
             <Button className="bg-tuna-50 text-tuna-950" onClick={handleSignIn}>
               Sign in

db380e6d1d8c47497279a29ff6602df5a1c5febb
Rogerio <rogeriopiatek@gmail.com>
2023-12-17 00:21:37 -0300

    chore: database preparation for like system

---
+++ prisma/migrations/20231217021617_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Like" DROP CONSTRAINT "Like_userId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
